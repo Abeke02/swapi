398c4ad4585339524f503cacb982dbe74631ddb1
Abdolla <abeke.002.ta@gmail.com>
2023-12-14 02:05:39 +0600

    show result

--- app.py
+++ app.py
@@ -1,16 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
 
+def get_film_titles(film_urls):
+    film_titles = []
+    for film_url in film_urls:
+        response = requests.get(film_url)
+        if response.status_code == 200:
+            film_info = response.json()
+            film_titles.append(film_info['title'])
+    return film_titles
+
+def get_planet_info(planet_url):
+    response = requests.get(planet_url)
+    if response.status_code == 200:
+        planet_info = response.json()
+        return planet_info
+    return None
+
+
 @app.route('/')
 def index():
-    # Здесь вы можете взаимодействовать с SWAPI и получить нужную информацию
-    # Например, получим информацию о первых 5 персонажах
-    response = requests.get('https://swapi.dev/api/people/')
-    characters = response.json()['results'][:5]
+    return render_template('index.html')
 
-    return render_template('index.html', characters=characters)
+
+@app.route('/result_for_planet/<path:planet_url>')
+def result_for_planet(planet_url):
+    planet_info = get_planet_info(planet_url)
+    
+    if planet_info:
+        return render_template('result_for_planet.html', planet_info=planet_info)
+    else:
+        return render_template('result_for_planet.html', error='Ошибка запроса')
+
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    try:
+        x = int(request.form['x'])
+        response = requests.get(f'https://swapi.dev/api/people/{x}/')
+
+        if response.status_code == 200:
+            character = response.json()
+            planet_info = get_planet_info(character['homeworld'])
+            film_titles = get_film_titles(character['films'])
+            return render_template('result.html', character=character, planet_info=planet_info, film_titles=film_titles)
+        else:
+            return render_template('result.html', error='Ошибка запроса')
+
+    except ValueError:
+        return render_template('index.html', error='Пожалуйста, введите корректное число.')
+
+@app.route('/result')
+def result():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
